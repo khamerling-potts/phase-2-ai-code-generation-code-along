03b9f56614ce9acd51427fea15c906aeacfad4f1
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-25 11:25:38 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+/** A Book object representation
+ * @typedef {Object} Book
+ * @property {string} title - The title of the book
+ * @property {string} author - The author of the book
+ * @property {string} genre - The genre of the book
+ */
+
+/**
+ * @param {string} genre - The genre of books required
+ * @param {Book[]} books - An array of Book objects
+ * @returns {Book[]} - An array of Book objects
+ */
+function filterByGenre(genre, books) {
+  return books.filter((book) => book.genre === genre);
+}
